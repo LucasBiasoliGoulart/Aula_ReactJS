3e96d672dffa5b04a10cb976530818ecee732c0f
LucasBiasoliGoulart <Biasoli1702@gmail.com>
2024-10-18 09:47:12 -0300

    aula_react

--- hello_world/src/App.js
+++ hello_world/src/App.js
@@ -1,44 +1,54 @@
 import logo from './logo.svg';
 import './App.css';
 
-function botao({texto}) {
+// Botão
+function Botao({textoBotao}) {
   return (
-    <button>Click aqui!</button>
+    <button className='link'>
+      {textoBotao}
+    </button>
   );
 }
 
-// Produtos
-const products = [
-  {title: 'Tomate', id: 1},
-  {title: 'Morango', id: 2},
-  {title: 'Alface', id: 3}
-]
-
-//function listaDeCompras() {
-//  const items = products.map(p =>);
-//}
-
-function App() {
+// Formulário
+function Formulario() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-
-        <botao texto={'Olá Mundo'}/>
-      </header>
+    <div className='principal'>
+      <div className='login'>
+        <h3>Login:</h3>
+        <input type='texto'></input>
+        <h3>Senha:</h3>
+        <input type='password'></input>
+        <Botao textoBotao={'Login'}></Botao>
+      </div>
+      <p className='texto'>&copy;2024 - Lucas Biasoli Goulart</p>
     </div>
   );
+}
+
+function App() {
+  let logado = true;
+  if (!logado) {
+    return (<Formulario/>)
+  }else {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+  
+          <h1 className='titulo'>Olá Mundo</h1>
+          <p className='texto'>Esse é meu primeiro site com ReactJS</p>
+  
+          <div className='botoes'>
+            <Botao textoBotao={'Click aqui'}></Botao>
+            <Botao textoBotao={'Saber mais'}></Botao>
+          </div>
+  
+          <p className='texto'>&copy;2024 - Lucas Biasoli Goulart</p>
+        </header>
+      </div>
+    )
+  }
 }
 
 export default App;
